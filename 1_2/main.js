e7b1a8956f54335948b3b977326a72427443e1df
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-09 14:47:30 +0100

    finish

---
+++ 1_2/main.js
@@ -0,0 +1,12 @@
+let array = [
+    3.14,
+    193.4464,
+    0.8596433607,
+    -2.940629089,
+]
+
+array.map((item) => {
+    let roundedNumber = Math.round(item)
+    console.log(roundedNumber)
+})
+
