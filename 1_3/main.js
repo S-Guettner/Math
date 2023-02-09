539b94bdcffbe33196ef3e6aba5c9b0a4cbe4c43
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-09 14:51:02 +0100

    finish

---
+++ 1_3/main.js
@@ -0,0 +1,8 @@
+let randomNum = Math.random()
+console.log(randomNum)
+
+let randomNum1_10 = Math.floor(Math.random() * 10) + 1
+console.log(randomNum1_10)
+
+let randomNum1_100 = Math.floor(Math.random() * 100) + 1
+console.log(randomNum1_100)
